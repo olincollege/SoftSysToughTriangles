6e31d34d123038c7c55eeda154abbd8d4ff62b20
AndrewCandy <adecandia@olin.edu>
2022-04-30 19:52:02 -0400

    Finalized command buffer and swap chain implementation.

--- vulkan_tutorial/first_app.hpp
+++ vulkan_tutorial/first_app.hpp
@@ -3,21 +3,43 @@
 #include "lve_window.hpp"
 #include "lve_pipeline.hpp"
 #include "lve_device.hpp"
+#include "lve_swap_chain.hpp"
+
+// std
+#include <memory>
+#include <vector>
+
 namespace lve {
 class FirstApp {
     public:
-        static constexpr int WIDTH = 800;
-        static constexpr int HEIGHT = 600;
+      static constexpr int WIDTH = 800;
+      static constexpr int HEIGHT = 600;
 
-        void run();
+      FirstApp();
+      ~FirstApp();
+
+      FirstApp(const FirstApp &) = delete;
+      FirstApp &operator=(const FirstApp &) = delete;
+
+      void run();
 
     private:
-        LveWindow lveWindow{WIDTH, HEIGHT, "Hello Vulkan!"};
-        LveDevice lveDevice{lveWindow};
-        LvePipeline lvePipeine{lveDevice,
-          "shaders/simple_shader.vert.spv",
-          "shaders/simple_shader.frag.spv",
-        LvePipeline::defaultPipelineConfigInfo(WIDTH, HEIGHT)};
+      void createPipelineLayout();
+      void createPipeline();
+      void createCommandBuffers();
+      void drawFrame();
+
+      LveWindow lveWindow{WIDTH, HEIGHT, "Hello Vulkan!"};
+      LveDevice lveDevice{lveWindow};
+      LveSwapChain lveSwapChain{lveDevice, lveWindow.getExtent()};
+      std::unique_ptr<LvePipeline> lvePipeline; //smart pointer
+      VkPipelineLayout pipelineLayout;
+      std::vector<VkCommandBuffer> commandBuffers;
+
+      // LvePipeline lvePipeine{lveDevice,
+      //   "shaders/simple_shader.vert.spv",
+      //   "shaders/simple_shader.frag.spv",
+      // LvePipeline::defaultPipelineConfigInfo(WIDTH, HEIGHT)};
 };
 
 } // namespace lve
